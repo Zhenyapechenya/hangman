12a22e48262f24fb6655f75531d121033dafe0b9
Zhenyapechenya <epsareva77@gmail.com>
2023-02-20 19:26:59 +0700

    add hangman man

--- classes.py
+++ classes.py
@@ -7,16 +7,101 @@ class Round:
         self.counter = 0
         self.fail_count = 6
         self.word = self.get_random_word()
-        self.hidden_output = len(self.word) * '_'
+        self.hidden_output = self.define_hidden_output()
         self.output_list = [
-            '0',
-            '1',
-            '2',
-            '3',
-            '4',
-            '5',
-            '6'
+            """
+   ==============
+   ||       |
+   ||       |       
+   ||      
+   ||    
+   ||   
+   ||      
+   ||      
+   ||     
+   ||  
+___||____________
+            """,
+            """
+   ==============
+   ||       |
+   ||       |       
+   ||      ( )
+   ||    
+   ||   
+   ||      
+   ||     
+   ||     
+   ||  
+___||____________
+            """,
+            """
+   ==============
+   ||       |
+   ||       |       
+   ||      ( )
+   ||     |   |
+   ||     |   | 
+   ||      ---
+   ||     
+   ||     
+   ||  
+___||____________
+            """,
+            """
+   ==============
+   ||       |
+   ||       |       
+   ||      ( )
+   ||    /|   |
+   ||   / |   | 
+   ||      ---
+   ||     
+   ||     
+   ||  
+___||____________    
+            """,
+            """
+   ==============
+   ||       |
+   ||       |       
+   ||      ( )
+   ||    /|   |\\
+   ||   / |   | \\
+   ||      ---
+   ||      
+   ||     
+   ||  
+___||____________
+            """,
+            """
+   ==============
+   ||       |
+   ||       |       
+   ||      ( )
+   ||    /|   |\\
+   ||   / |   | \\
+   ||      ---
+   ||      / 
+   ||     /  
+   ||  
+___||____________
+            """,
+            """
+   ==============
+   ||       |
+   ||       |       
+   ||      ( )
+   ||    /|   |\\
+   ||   / |   | \\
+   ||      ---
+   ||      / \\
+   ||     /   \\
+   ||  
+___||____________
+            """
         ]
+
 
     def get_random_word(self):
         words_file = open("words.txt", "r")
@@ -26,6 +111,13 @@ class Round:
                 words_list.append(w.strip().lower())
         words_file.close()
         return random.choice(words_list)
+
+
+    def define_hidden_output(self):
+        output = []
+        for i in range(len(self.word)):
+            output.append('_')
+        return output
 
 
     def check_position(self, letter):
@@ -43,43 +135,31 @@ class Round:
         else:
             round.counter += 1
         print(self.output_list[self.counter])
-        print(self.hidden_output)
 
-
-    # def change_hangman(self):
-    #     round.counter += 1
-    #     print(self.output_list[self.counter])
-    #     print(self.hidden_output)
-
-
-    # def change_hidden_output(self, indexes):
-    #     for index in indexes:
-    #         self.hidden_output[index] = self.word[index]
-    #     print(self.output_list[self.counter])
-    #     print(self.hidden_output)
+        for elem in self.hidden_output:
+            print(elem, end=' ')
 
 
     def check_win(self):
-        if self.hidden_output == self.word:
-            return True
+        print('\n', ''.join(self.hidden_output))
+        print('\n', self.word)
+        if ''.join(self.hidden_output) == self.word:
+            return  True
         else:
             return False
-        
 
-    def check_fail(self):
-        pass
+
 
 
 round = Round()
-
-while round.counter < round.fail_count or round.check_win():
+print(round.output_list[0])
+while round.counter < round.fail_count and not round.check_win():
     letter = input('Введите букву:')
     round.change_output(letter)
-    # letter = input('Введите букву:')
-    # if letter in round.word:
-    #     list_of_indexes = round.check_position(letter)
-    #     round.change_hidden_output(list_of_indexes)
-    # else:
-    #     round.change_hangman()
+
+if round.check_win():
+    print('win')
+else:
+    print('fail')
 
 
