b454698b46c93065dc39cffc3115ac1ca43e86bb
Zhenyapechenya <epsareva77@gmail.com>
2023-03-02 18:44:55 +0700

    add README

--- src/hangman.py
+++ src/hangman.py
@@ -3,14 +3,15 @@ from Round import Round
 
 try:
     round = Round()
+    print("\033[H\033[J")
     print(round.output_list[0])
     
     while round.counter < round.fail_count and not round.check_win():
-        letter = input('\nЗагадано слово на русском языке. Введите букву:')
+        letter = input('\n\nЗагадано слово на русском языке. Введите букву:')
         round.change_output(letter)
 
     if round.check_win():
-        print('\nПоздравляю с победой!')
+        print('\n\nПоздравляю с победой!')
     else:
         print('\n\nВы проиграли. Загаданное слово:', round.word)
 
