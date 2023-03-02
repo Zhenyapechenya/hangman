c682108e0c270d785e92c99d374f27953c666ea0
Zhenyapechenya <epsareva77@gmail.com>
2023-03-02 16:23:47 +0700

    /usr/local/bin/python3

---
+++ Round.py
@@ -0,0 +1,180 @@
+import random
+
+
+class Round:
+
+    def __init__(self):
+        self.counter = 0
+        self.fail_count = 8
+        self.word = self.get_random_word()
+        self.hidden_output = self.define_hidden_output()
+        self.output_list = [
+            """
+   ==============
+   ||       |
+   ||       |       
+   ||      
+   ||    
+   ||   
+   ||      
+   ||      
+   ||     
+   ||  
+___||____________
+            """,
+            """
+   ==============
+   ||       |
+   ||       |       
+   ||      ( )
+   ||    
+   ||   
+   ||      
+   ||     
+   ||     
+   ||  
+___||____________
+            """,
+            """
+   ==============
+   ||       |
+   ||       |       
+   ||      ( )
+   ||     |   |
+   ||     |   | 
+   ||      ---
+   ||     
+   ||     
+   ||  
+___||____________
+            """,
+            """
+   ==============
+   ||       |
+   ||       |       
+   ||      ( )
+   ||    /|   |
+   ||   / |   | 
+   ||      ---
+   ||     
+   ||     
+   ||  
+___||____________    
+            """,
+            """
+   ==============
+   ||       |
+   ||       |       
+   ||      ( )
+   ||    /|   |\\
+   ||   / |   | \\
+   ||      ---
+   ||      
+   ||     
+   ||  
+___||____________
+            """,
+            """
+   ==============
+   ||       |
+   ||       |       
+   ||      ( )
+   ||    /|   |\\
+   ||   / |   | \\
+   ||      ---
+   ||      / 
+   ||      
+   ||  
+___||____________
+            """,
+            """
+   ==============
+   ||       |
+   ||       |       
+   ||      ( )
+   ||    /|   |\\
+   ||   / |   | \\
+   ||      ---
+   ||      / 
+   ||     /   
+   ||  
+___||____________
+            """,
+            """
+   ==============
+   ||       |
+   ||       |       
+   ||      ( )
+   ||    /|   |\\
+   ||   / |   | \\
+   ||      ---
+   ||      / \\
+   ||     /   
+   ||    
+___||____________
+            """,
+            """
+   ==============
+   ||       |
+   ||       |       
+   ||      ( )
+   ||    /|   |\\
+   ||   / |   | \\
+   ||      ---
+   ||      / \\
+   ||     /   \\
+   ||    
+___||____________
+            """
+        ]
+
+
+    def get_random_word(self):
+        try:
+            words_file = open("words.txt", "r")
+            words_list = []
+            for w in words_file.readlines():
+                if w != '\n':
+                    words_list.append(w.strip().lower())
+            words_file.close()
+            return random.choice(words_list)
+        except:
+            print("Invalid")
+
+
+    def define_hidden_output(self):
+        output = []
+        for i in range(len(self.word)):
+            output.append('_')
+        return output
+
+
+    def check_position(self, letter):
+        indexes = []
+        for i in range(len(self.word)):
+            if letter == self.word[i]:
+                indexes.append(i)
+        return indexes
+
+
+    def change_output(self, letter):
+        print("\033[H\033[J")
+        if letter in self.word:
+            for index in self.check_position(letter):
+                self.hidden_output[index] = self.word[index]
+        else:
+            self.counter += 1
+        print(self.output_list[self.counter])
+
+        for elem in self.hidden_output:
+            print(elem, end=' ')
+
+
+    def check_win(self):
+        if ''.join(self.hidden_output) == self.word:
+            return  True
+        else:
+            return False
+
+
+
